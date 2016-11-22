621bbb375eb3d766e98ddf4bcac507079690c9db
Zinksta <mzink79@gmail.com>
2016-11-22 13:52:03 +1000

    Add project files.

---
+++ ADS_MergeSort_Algorithm/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ADS_MergeSort_Algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(12, 28);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 0;
+            this.btnExecute.Text = "&Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(93, 28);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(479, 292);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(497, 326);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnExecute;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnExecute);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MergeSort Algorithm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ ADS_MergeSort_Algorithm/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADS_MergeSort_Algorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /*--------------MergeSort Algorithm-----------------------
+        The steps of the MergeSort algorithm can be listed as follows:
+        1. Divide the unsorted array into two sub arrays
+        2. Recursively sort each sub array
+        3. Merge the two sub arrays by comparing values
+        */
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            // declare an array
+            int[] numbers = new int[] { 3, 6, 1, 4, 5 };
+
+            // add text to output
+            txtOutput.Text = "Unsorted array:\r\n";
+
+            // loop through array and print each value
+            foreach (int value in numbers)
+                txtOutput.Text += value + "\t";
+
+            // add text to output
+            txtOutput.Text += "\r\nSorted array:\r\n";
+
+            //call mergeSort method
+            mergeSort(numbers, 0, numbers.Length - 1);
+
+            // loop through array after it has been sorted and print each value
+            foreach (int value in numbers)
+                txtOutput.Text += value + "\t";
+        }
+        /* MergerSort Algorithm will divide the array
+            1st parameter is the array to sort, 2nd is the starting index(usually 0),
+            the 3rd is the upperbound of the array(which is usually the last index)
+            example:- a numbers array with 5 indexs would be mergeSort(numbers, 0, numbers.Length - 1)
+            *******THIS METHOD WILL NOT WORK WITHOUT THE MERGE METHOD********
+        */
+        public void mergeSort(int[] sortArray, int lower, int upper)
+        {
+            int middle;
+            if (upper == lower)
+                return;
+            else
+            {
+                middle = (lower + upper) / 2;
+                mergeSort(sortArray, lower, middle);
+                mergeSort(sortArray, middle + 1, upper);
+                Merge(sortArray, lower, middle + 1, upper);
+            }
+        }
+        // Merge method will merge subarrays together incrementally
+        // ******THIS METHOD WILL NOT WORK WITHOUT THE MERGESORT METHOD********
+        public void Merge(int[] sortArray, int lower, int middle, int upper)
+        {
+            int[] temp = new int[sortArray.Length];
+            int lowEnd = middle - 1;
+            int low = lower;
+            int n = upper - lower + 1;
+
+            while ((lower <= lowEnd) && (middle <= upper))
+            {
+                if (sortArray[lower] <= sortArray[middle])
+                {
+                    temp[low] = sortArray[lower];
+                    low++;
+                    lower++;
+                }
+                else
+                {
+                    temp[low] = sortArray[middle];
+                    low++;
+                    middle++;
+                }
+            }
+            while (lower <= lowEnd)
+            {
+                temp[low] = sortArray[lower];
+                low++;
+                lower++;
+            }
+            while (middle <= upper)
+            {
+                temp[low] = sortArray[middle];
+                low++;
+                middle++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sortArray[upper] = temp[upper];
+                upper--;
+            }
+        }
+    }
+}
